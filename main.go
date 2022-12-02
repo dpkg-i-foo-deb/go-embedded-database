0dc82d2c7889d205d5d92ac6fb56560f58f90307
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-12-02 14:02:48 -0500

    Try to improve main

--- main.go
+++ main.go
@@ -4,19 +4,30 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"math/rand"
+	"time"
 
 	_ "github.com/glebarez/go-sqlite"
 )
 
 func main() {
 
+	var query string
 	var result int
+
+	rand.Seed(time.Now().UnixNano())
+
+	query = fmt.Sprintf("INSERT INTO TEST (id) VALUES (%v)", rand.Int())
 	// connect
 	db, err := sql.Open("sqlite", "datos.db")
 
 	handleError(err)
 
 	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY);`)
+
+	handleError(err)
+
+	_, err = db.Exec(query)
 
 	handleError(err)
 
